d65fdf1e6628bf6e83f0814c855fc453c24798bc
Waldorfio <walid.hanifi707@gmail.com>
2022-08-17 20:08:52 +1000

    CSS refactor to include checkbox hover, rather than control via JS function

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ console.clear();
 console.log('test console log');
 
 checkbox = document.getElementById('todo-checkbox');
-checkbox.addEventListener('click', () => checkboxHover());
+checkbox.addEventListener('click', () => checkboxClicked());
 
-function checkboxHover() {
-    if (checkbox.innerHTML=='') {checkbox.innerHTML='✓'; return};
-    if (checkbox.innerHTML=='✓') {checkbox.innerHTML=''; return};
+function checkboxClicked() {
+    console.log('a checkbox was clicked');
 }
 
 console.log('----------');
