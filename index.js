13ddde790b8e6455a52f5e2b4052fd6f42536863
Waldorfio <walid.hanifi707@gmail.com>
2022-08-18 18:20:38 +1000

    Add js new list functionality to button

--- index.js
+++ index.js
@@ -9,4 +9,32 @@ function checkboxClicked() {
     console.log('a checkbox was clicked');
 }
 
+
+
+listArr = []
+
+function listFactory(projectName, colour) { // Creates "new list" objects
+    return {
+        projectName: projectName,
+        colour: colour,
+    }
+
+}
+
+newContainer = document.getElementById('new-container');
+newContainer.addEventListener('click', () => newContainerClicked());
+
+function newContainerClicked() {
+    newList = listFactory('test12','red');
+    createList('test12','red');
+    listArr.push(newList);
+}
+
 console.log('----------');
+
+function createList(projectName, colour) {
+    list = document.createElement('div');
+    list.id = 'project-template';
+    list.innerHTML = projectName;
+    document.getElementById('project-list-container').appendChild(list);
+}
