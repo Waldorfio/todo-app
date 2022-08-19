7f4b5b0b9b91c5db3e14d18d936ad9343c132cb9
Waldorfio <walid.hanifi707@gmail.com>
2022-08-19 16:25:19 +1000

    Add function drop down

--- index.js
+++ index.js
@@ -25,28 +25,21 @@ newContainer = document.getElementById('new-container');
 newContainer.addEventListener('click', () => newContainerClicked());
 
 function newContainerClicked() {
-    newList = listFactory('test12','red');
-    createList('test12','red');
+    newList = listFactory('test12', 'orange');
+    createList('test12', 'orange');
     listArr.push(newList);
 }
 
 console.log('----------');
 let i = 1;
 function createList(projectName, colour) {
-    // list = document.createElement('input');
-    // list.id = 'project-template';
-    // list.type = 'text';
-    // list.name = 'project-template';
-    // list.value = 'Untitled-list '+listIndex;
-    // document.getElementById('project-list-container').appendChild(list);
-
-
     divContainer = document.createElement('div');
     divContainer.id = 'project-template-container';
     
     stackLogo = document.createElement('span');
     stackLogo.className = 'material-symbols-outlined';
     stackLogo.innerHTML = 'menu';
+    stackLogo.style['color'] = colour;
     stackLogo.id = 'stack-logo'+listIndex;
 
     list = document.createElement('input');
@@ -66,4 +59,42 @@ function createList(projectName, colour) {
     listIndex = listIndex + 1;
 
     console.log('created '+listIndex);
+}
+
+
+
+let hiddenTodo = false;
+toCompId = document.getElementById('to-complete-id');
+toCompId.addEventListener('click', () => collapseTodo());
+function collapseTodo() {
+    if (hiddenTodo==false) {
+        document.getElementById('todo-card').style.display = 'none';
+        document.getElementById('todo-id-logo').innerHTML = 'navigate_next';
+        hiddenTodo = true; 
+        return
+    }
+    if (hiddenTodo==true) {
+        document.getElementById('todo-card').style.display = '';
+        document.getElementById('todo-id-logo').innerHTML = 'expand_more';
+        hiddenTodo = false;
+        return
+    }
+}
+
+let hiddenComp = false;
+compId = document.getElementById('completed-id');
+compId.addEventListener('click', () => collapseComp());
+function collapseComp() {
+    if (hiddenComp==false) {
+        document.getElementById('comp-card').style.display = 'none';
+        document.getElementById('completed-id-logo').innerHTML = 'navigate_next';
+        hiddenComp = true; 
+        return
+    }
+    if (hiddenComp==true) {
+        document.getElementById('comp-card').style.display = '';
+        document.getElementById('completed-id-logo').innerHTML = 'expand_more';
+        hiddenComp = false;
+        return
+    }
 }
