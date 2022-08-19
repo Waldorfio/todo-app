1bc809b8ba410dffaf85c6b6d26da477d518893a
Waldorfio <walid.hanifi707@gmail.com>
2022-08-19 19:50:50 +1000

    Create basic tab switch feature

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 console.clear();
 
-console.log('test console log');
 
-checkbox = document.getElementById('todo-checkbox');
-checkbox.addEventListener('click', () => checkboxClicked());
+// -------- INITIALISING VARIABLES  -------- //
 
-function checkboxClicked() {
-    console.log('a checkbox was clicked');
-}
+let listIndex = 0;      // Stores the index of lists created
 
 
-listArr = []
-let listIndex = 1;      // Stores the index of lists created
+// -------- FACTORY FUNCTIONS  -------- //
 
-function listFactory(projectName, colour) { // Creates "new list" objects
+
+containerLog = [];
+function containerFactory(projectName, colour) {
     return {
         projectName: projectName,
         colour: colour,
     }
-
 }
+
+todoLog = [];
+function todoFactory(itemName, subItems, dueDate, status) {
+    return {
+        itemName: itemName,
+        subItems: subItems,
+        dueDate: dueDate,
+        status: status,
+    }
+}
+
+// -------- FUNCTIONS  -------- //
+
+
+    // Create new list
 
 newContainer = document.getElementById('new-container');
-newContainer.addEventListener('click', () => newContainerClicked());
+newContainer.addEventListener('click', function() {
+    newList = containerFactory('placeholder', 'orange');
+    createList('placeholder', 'orange');
+    containerLog.push(newList);
+});
 
-function newContainerClicked() {
-    newList = listFactory('test12', 'orange');
-    createList('test12', 'orange');
-    listArr.push(newList);
-}
 
-console.log('----------');
 let i = 1;
 function createList(projectName, colour) {
     divContainer = document.createElement('div');
@@ -43,10 +52,11 @@ function createList(projectName, colour) {
     stackLogo.id = 'stack-logo'+listIndex;
 
     list = document.createElement('input');
-    list.id = 'project-template';
+    list.id = 'item'+listIndex;
+    list.className = 'project-template';
     list.type = 'text';
-    list.name = 'project-template';
-    list.value = 'Untitled-list '+listIndex;
+    list.name = projectName;
+    list.value = projectName+listIndex;
 
     // Append new elements to container
     document.getElementById('project-list-container').appendChild(divContainer);
@@ -58,11 +68,29 @@ function createList(projectName, colour) {
     list.select();              // Highlights the new list
     listIndex = listIndex + 1;
 
-    console.log('created '+listIndex);
+    // Update object if list name changes
+    list.addEventListener("change", listChange.bind(this, list, listIndex));
+    function listChange(list,listIndex) {
+        console.log(list.value);
+        containerLog[listIndex-1].projectName = list.value;
+    }
+
+    // Wiping content per tab change
+    divContainer.addEventListener("click", switchTab.bind(this, listIndex));
+    function switchTab(listIndex) {
+        console.log('tab switching...');
+        obj = containerLog[listIndex-1];
+        document.getElementById('project').innerHTML = obj.projectName;
+        document.getElementById('project').style['color'] = obj.colour;
+        
+    }
+
 }
 
 
+    // Collapse Completed/Incomplete tasks
 
+// Controls incomplete tasks
 let hiddenTodo = false;
 toCompId = document.getElementById('to-complete-id');
 toCompId.addEventListener('click', () => collapseTodo());
@@ -80,7 +108,7 @@ function collapseTodo() {
         return
     }
 }
-
+// Controls completed tasks
 let hiddenComp = false;
 compId = document.getElementById('completed-id');
 compId.addEventListener('click', () => collapseComp());
@@ -97,4 +125,14 @@ function collapseComp() {
         hiddenComp = false;
         return
     }
+}
+
+    // Check off items from list
+
+checkbox = document.getElementById('todo-checkbox');
+checkbox.addEventListener('click', () => checkboxClicked());
+
+function checkboxClicked() {
+    console.log('a checkbox was clicked');
+    // LOGIC TO CHANGE 'status' of relevant todo item
 }
